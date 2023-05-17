ae09da9ab1d4dbdaa3b7de7b88d877d50e42fccb
Sanket Ghosh <code.sanket13@gmail.com>
2023-05-17 19:21:39 +0530

    ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,20 +5,29 @@ import { useState } from "react";
 
 const Navbar = () => {
   return (
-    <nav className="sticky z-10 top-7 rounded-sm">
-      {/* container  */}
-      <div className="px-4 py-6 flex items-center justify-between">
-        <div className="w-1/2 flex border-[2px] border-gray-300 rounded-md items-center gap-2 px-2">
+    <div className="relative w-full bg-white">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+        <div className="w-full">
           <input
+            className="flex h-10 w-1/2 rounded-md bg-gray-100 px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
             type="text"
-            className="py-2 px-3 outline-none w-full bg-transparent"
-          />
-          <button>
-            <FaSearch />
-          </button>
+            placeholder="Serach"
+          ></input>
         </div>
+        <div className="ml-2 mt-2 hidden lg:block">
+          <span className="relative inline-block">
+            <img
+              className="h-10 w-10 rounded-full"
+              src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg"
+              alt="Dan_Abromov"
+            />
+          </span>
+        </div>
+        {/* <div className="ml-2 lg:hidden">
+          <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
+        </div> */}
       </div>
-    </nav>
+    </div>
   );
 };
 
